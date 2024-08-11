b8180dd8fc09ffdae0b6824f15f6cf76450fbc9f
Kuzivakwashe Muvezwa <muvezwakuzivakwashe@yahoo.com>
2024-08-11 04:02:02 +0200

    first push

---
+++ src/app/page.tsx
@@ -0,0 +1,100 @@
+import Image from "next/image";
+import { ConnectButton } from "thirdweb/react";
+import based from "@public/based.jpg";
+import { accountAbstraction, client } from "./constants";
+import Link from "next/link";
+import { optimismSepolia } from "thirdweb/chains";
+
+export default function Home() {
+	return (
+		<div className="py-20">
+			<Header />
+
+			<div className="flex justify-center mb-20">
+				<ConnectButton
+					client={client}
+					accountAbstraction={accountAbstraction}
+					chain={optimismSepolia}
+				/>
+			</div>
+
+			<Menu />
+
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<header className="flex flex-col items-center mb-20 md:mb-20">
+			<Image
+				src={based}
+				alt=""
+				width={120}
+				style={{
+					filter: "drop-shadow(0px 0px 24px #a726a9a8)",
+				}}
+			/>
+
+			<br/>
+
+			<h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
+				Based Trivia
+			</h1>
+
+			<p className="text-zinc-300 text-base">
+				Test your blockchain knowledge to see if you can answer Web3’s most savage questions.
+				{/* <a
+					className="text-purple-400"
+					target="_blank"
+					href="https://portal.thirdweb.com/connect"
+				>
+					Connect SDK
+				</a> */}
+				.
+			</p>
+		</header>
+	);
+}
+
+function Menu() {
+	return (
+		// <div className="grid gap-4 lg:grid-cols-3 justify-center">
+		<div className="justify-center">
+			<MenuItem
+				title="Join Quiz"
+				href="/quiz"
+				description="Execute transactions on different chains maintaining the same smart account address."
+			/>
+		</div>
+	);
+}
+
+function MenuItem(props: { title: string; href: string; description: string }) {
+	return (
+		<Link
+			href={props.href}
+			className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
+		>
+			<article>
+				<h2 className="text-lg font-semibold mb-2">{props.title}</h2>
+				<p className="text-sm text-zinc-400">{props.description}</p>
+			</article>
+		</Link>
+	);
+}
+
+function Footer() {
+	return (
+		<div className="flex flex-col items-center mt-20">
+			<Link
+				className="text-center text-sm text-gray-400"
+				target="_blank"
+				href="https://github.com/thirdweb-example/account-abstraction"
+			>
+				View code on GitHub
+			</Link>
+		</div>
+	);
+}
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+import React from "react";
+import { balanceOf, claimTo, getNFT } from "thirdweb/extensions/erc1155";
+import {
+	ConnectButton,
+	MediaRenderer,
+	TransactionButton,
+	useActiveAccount,
+	useReadContract,
+} from "thirdweb/react";
+import { accountAbstraction, client, editionDropTokenId } from "../constants";
+import Link from "next/link";
+import { baseSepolia, optimismSepolia } from "thirdweb/chains";
+import { ThirdwebContract, getContract, defineChain } from "thirdweb";
+
+const Quiz: React.FC = () => {
+	const smartAccount = useActiveAccount();
+	const modeTestnet = defineChain(919);
+
+	return (
+		<div className="flex flex-col items-center">
+			<h1 className="text-center text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-12 text-zinc-100">
+				Based Trivia
+			</h1>
+			<ConnectButton
+				client={client}
+				accountAbstraction={accountAbstraction}
+				connectModal={{
+					size: "compact",
+				}}
+			/>
+
+			<div className="flex flex-row">
+				<NFTClaimer
+					receiverAddress={smartAccount?.address}
+					dropContract={getContract({
+						address: "0xbD658E72Ecb1248C0996b457E30e2f16D8dD132e",
+						chain: optimismSepolia,
+						client,
+					})}
+					tokenId={0n}
+				/>
+				<div className="h-auto w-[1px] bg-gray-600 mx-12 mt-8" />
+				<NFTClaimer
+					receiverAddress={smartAccount?.address}
+					dropContract={getContract({
+						address: "0x8b3Cb2b05Fde1b65e4261987c438B934b24Cba4B",
+						chain: baseSepolia,
+						client,
+					})}
+					tokenId={0n}
+				/>
+				<div className="h-auto w-[1px] bg-gray-600 mx-12 mt-8" />
+				<NFTClaimer
+					receiverAddress={smartAccount?.address}
+					dropContract={getContract({
+						address: "0x78af091d42Df4b6C7bE8683d8C0B5BEF174aC608",
+						chain: modeTestnet,
+						client,
+					})}
+					tokenId={0n}
+				/>
+			</div>
+
+			<Link href={"/"} className="text-sm text-gray-400 mt-8">
+				Back to menu
+			</Link>
+		</div>
+	);
+};
+
+type NFTClaimerProps = {
+	receiverAddress?: string;
+	dropContract: ThirdwebContract;
+	tokenId: bigint;
+};
+
+const NFTClaimer: React.FC<NFTClaimerProps> = (props: NFTClaimerProps) => {
+	const { data: nft, isLoading: isNftLoading } = useReadContract(getNFT, {
+		contract: props.dropContract,
+		tokenId: props.tokenId,
+	});
+	const { data: ownedNfts } = useReadContract(balanceOf, {
+		contract: props.dropContract,
+		owner: props.receiverAddress!,
+		tokenId: props.tokenId,
+		queryOptions: { enabled: !!props.receiverAddress },
+	});
+	return (
+		<div className="flex flex-col my-8">
+			{isNftLoading ? (
+				<div className="w-full mt-24">Loading...</div>
+			) : (
+				<>
+					{nft ? (
+						<MediaRenderer client={client} src={nft.metadata.image} />
+					) : null}
+					{props.receiverAddress ? (
+						<>
+							<p className="font-semibold text-center my-2">
+								You answered {ownedNfts?.toString() || "0"} on{" "}
+								{props.dropContract.chain.name}
+							</p>
+							<TransactionButton
+								transaction={() =>
+									claimTo({
+										contract: props.dropContract,
+										tokenId: props.tokenId,
+										to: props.receiverAddress!,
+										quantity: 1n,
+									})
+								}
+								onError={(error) => {
+									alert(`Error: ${error.message}`);
+								}}
+								onTransactionConfirmed={async () => {
+									alert("Claim successful!");
+								}}
+							>
+								Select
+							</TransactionButton>
+						</>
+					) : (
+						<p className="text-center mt-8">
+							Login to answer on {props.dropContract.chain.name}!
+						</p>
+					)}
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Quiz;
